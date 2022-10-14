f957c9332e15a224219441d3639b31cb9b2c9523
cvki <1016140016@qq.com>
2022-10-14 11:27:16 +0800

    VS-torch10Tst

---
+++ testEnv/torch10Tst.py
@@ -0,0 +1,16 @@
+import torch
+import cv2
+import numpy as np
+from matplotlib import pyplot
+import pandas as pd
+
+print(torch.__version__)
+print(torch.version.cuda)
+print(torch.cuda.is_available())
+print(torch.cuda.device_count())
+
+path='/d2/code/testEnv/rose.png'
+rose=cv2.imread(path)
+cv2.imshow('rose',rose)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
